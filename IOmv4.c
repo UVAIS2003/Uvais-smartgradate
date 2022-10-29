fb172d435f319d945c2dc1df945eee8658b67978
MOHAMMED UVAIS MOHAJIR <113407744+UVAIS2003TNC@users.noreply.github.com>
2022-10-29 13:06:10 +0530

    C

---
+++ IOmv4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+34
+ Compiled and Presented by – M.H. Shoiab.
+void main()
+{
+ int a, b, c;
+ printf("Enter two numbers to add\n");
+ scanf("%d%d",&a,&b);
+ c = a + b;
+ printf("Sum of entered numbers = %d\n",c);
+
+}
