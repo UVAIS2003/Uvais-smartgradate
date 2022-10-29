1fa86a14bd4f2fec3db6d497d3aefe2b3e532886
MOHAMMED UVAIS MOHAJIR <113407744+UVAIS2003TNC@users.noreply.github.com>
2022-10-29 12:44:15 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package Closedbookjava;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ Resizingarray.java
@@ -0,0 +1,7 @@
+package Closedbookjava;
+
+public class Resizingarray {
+    public static void main(String[] args) {
+        System.out.println("We cannot resize array in java");
+    }
+}
